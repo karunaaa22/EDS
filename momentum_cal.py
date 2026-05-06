f3c42fde47b31660d0329666c28976dfff309a35
karunaaa22 <karunazanjurne@gmail.com>
2026-05-07 00:48:45 +0530

    Add momentum calculation functionality

---
+++ momentum_cal.py
@@ -0,0 +1,6 @@
+mass = float(input("Enter mass in kg:")) #input for mass
+velocity = float(input("Enter velocity in m/s:"))#input for velocity
+
+momentum = mass * velocity #valculation of momentum
+
+print(f"{momentum:.2f}")
