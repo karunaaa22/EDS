06848e1cb794293248d7000d799efddce86a9f43
karunaaa22 <karunazanjurne@gmail.com>
2026-05-07 00:44:52 +0530

    Add age classification based on user input

---
+++ age.py
@@ -0,0 +1,12 @@
+age=int(input("enter your age:"))
+if 0<age<5:
+    print("toddler")
+elif 5<age<12:
+    print("child")
+elif 12<age<18:
+    print("teenager")
+elif 18<age<50:
+    print("adult")
+else:
+    print("senior citizen")
+    
