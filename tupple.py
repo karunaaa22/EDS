8d7ce5a2cb65705ce1925e613c8604a2743fb3f7
karunaaa22 <karunazanjurne@gmail.com>
2026-05-07 00:58:43 +0530

    Implement tuple operations in tupple.py
    Added a program to demonstrate tuple operations including printing elements, calculating length, finding maximum value, and counting occurrences.

---
+++ tupple.py
@@ -0,0 +1,18 @@
+#Tupple Program
+
+n = (67,78,67,75,84,47)
+
+#printing all element in tupple
+for i in n:
+    print(i)
+
+#length of tupple
+print(f"Length of tupple is: {len(n)}")
+
+#maximum value in tupple
+m = max(n)
+print(f"Maximum Value of Tupple: {m}")
+
+#Counting occurence of element
+
+print("count of 67 is:", n.count(67))
